29c22ba6ff16b5c343127b522ec47fadd3bc6835
kosaka-akiyoshi <knzm25813@gmail.com>
2020-03-07 11:47:10 +0900

    課題再提出 PHP - 四則演算

--- taskClac/taskClac.php
+++ taskClac/taskClac.php
@@ -10,16 +10,10 @@
     <title>taskCalc</title>
 </head>
 <body>
-    <p><?php
-            echo( "( 1 + 3 ) * 5 = ". ( 1 + 3 ) * 5 ); ?></p>
-    <p><?php
-            echo( "round( 22 / 3 , 2 ) = ". round( 22 / 3 , 2 ) ); ?></p>
-    <p><?php
-            echo( "ceil( 22 / 3 ) = ". ceil( 22 / 3 ) ); ?></p>
-    <p><?php
-            echo( "floor( 22 / 3 ) = ". floor( 22 / 3 ) ); ?></p>
-    <p><?php
-            echo( "round( 5 / 36 , 1 ) = " 
-            . round( 5 / 36 , 1 )); ?></p>  
+    <p><?php echo( "( 1 + 3 ) * 5 = ". ( 1 + 3 ) * 5 ); ?></p>
+    <p><?php echo( "round( 22 / 3 , 2 ) = ". round( 22 / 3 , 2 ) ); ?></p>
+    <p><?php echo( "ceil( 22 / 3 ) = ". ceil( 22 / 3 ) ); ?></p>
+    <p><?php echo( "floor( 22 / 3 ) = ". floor( 22 / 3 ) ); ?></p>
+    <p><?php echo( "round( 5 / 36 , 1 ) = " . round( 5 / 36 , 1 )); ?></p>  
 </body>
 </html>
