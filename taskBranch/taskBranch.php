c07a589d89895639272768dfa911b90a1d19a9ca
kosaka-akiyoshi <knzm25813@gmail.com>
2020-03-07 15:54:10 +0900

    課題提出 PHP - 条件文

--- taskBranch/taskBranch.php
+++ taskBranch/taskBranch.php
@@ -1,6 +1,4 @@
 
-<?php
-    $string = "100" ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -10,16 +8,95 @@
     <title>taskBranch</title>
 </head>
 <body>
-    <p><?php 
-    //課題１－１
-    $number = 100;
-    if($variable == $number){
-        echo("\$string100は100です")
-    }　else {
-        echo("variableの値は100ではありません")
-    }
-    ?><p>
-</body>
-<body>
+        <?php $string = "100"; $number = 100; ?>
+    <p>
+        <?php //task 1 - 1
+        if($string == $number){
+        echo("\$string100は100です。<br>");
+        } else {
+        echo("\$string100は100ではありません。<br>");
+        } ?>
+
+        <?php //task 1 - 2
+        if($string === $number){
+        echo("\$string100は100です。<br>");
+        } else {
+        echo("\$string100は100ではありません。<br>");
+        } ?>
+
+        <?php //task 1 - 3
+        if($string > $number){
+        echo("\$string100は101以上です。<br>");
+        } else {
+        echo("\$string100は101以上ではありません。<br>");
+        } ?>
+
+        <?php //task 1 - 4
+        if($string >= $number){
+        echo("\$string100は100以上です。<br>");
+        } else {
+        echo("\$string100は100以上ではありません。<br>");
+        } ?>
+    </p>
+        <?php 
+        $language = rand( 1,100 ); 
+        $math = rand( 1,100 );
+        $science = rand( 1,100 );
+        $society = rand( 1,100 );
+        $english = rand( 1,100 );
+        $Grade = ( $language + $math + $science + $society + $english ) / 5;
+        $allGrade = 67.3;
+        ?>
+    <p> 
+        <?php //task 2 - 1
+         echo("A君の平均点は" . $Grade . "点です。<br>"); 
+        ?>
+        <?php //task 2 - 2
+         if($Grade >= $allGrade){
+            echo("A君の平均点は67.3以上です。<br>");
+            } else {
+            echo("A君の平均点は67.3未満です。<br>");
+            } 
+        ?>
+    </p>
+        <?php $case = '1'; ?>
+    <p> 
+        <?php //task 3 - 1
+            switch($case){
+                case true:
+                    echo 'bool<br>';
+                    break;
+
+                case 1:
+                    echo 'int<br>';
+                    break; 
+            
+                case '1':
+                    echo 'string<br>';
+                    break;
+            
+                default:
+                    echo 'other<br>';
+            }
+        ?>
+        <?php //task 3 - 2
+            switch($case){
+                case $case === true: //false 
+                    echo 'bool<br>';
+                    break;
+
+                case $case === 1: //false
+                    echo 'int<br>';
+                    break; 
+        
+                case $case === '1'://true
+                    echo 'string<br>';
+                    break;
+        
+                default:
+                    echo 'other<br>';
+        } 
+        ?>
+    </p>
 </body>
 </html>
