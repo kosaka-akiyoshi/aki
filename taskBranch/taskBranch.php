6e731bb100517322798e168e192057ad1e118a80
kosaka-akiyoshi <knzm25813@gmail.com>
2020-03-07 11:44:43 +0900

    課題再提出 PHP - 四則演算

---
+++ taskBranch/taskBranch.php
@@ -0,0 +1,25 @@
+
+<?php
+    $string = "100" ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>taskBranch</title>
+</head>
+<body>
+    <p><?php 
+    //課題１－１
+    $number = 100;
+    if($variable == $number){
+        echo("\$string100は100です")
+    }　else {
+        echo("variableの値は100ではありません")
+    }
+    ?><p>
+</body>
+<body>
+</body>
+</html>
