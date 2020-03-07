7112b945f072a3752eb04f60a07652b60d80692d
kosaka-akiyoshi <knzm25813@gmail.com>
2020-03-07 15:56:05 +0900

    その他

---
+++ dataType/TryDataType.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>taskDataType</title>
+</head>
+<body>
+    <p><?php
+    $doubleString = "Hello, \"World\"!";
+    echo("$doubleString"); ?></p>
+</body>
+</html>
---
+++ taskBranch/tryBranch.php
@@ -0,0 +1,15 @@
+
+    <p><?php
+    $variable = 100;
+    if($variable == 100){
+        echo("variableの値は100です<br>");
+    }
+    echo("処理終了！");    ?></p>
+    <p><?php
+    $variable = 200;   
+    if($variable == 100){
+        echo("variableの値は100です<br>");
+    }
+    echo("処理終了！");
+    ?></p>
+    
