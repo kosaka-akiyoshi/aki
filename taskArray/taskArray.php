ffbf158e9b153740306c2b372f55a340b331e2bc
kosaka-akiyoshi <knzm25813@gmail.com>
2020-03-08 11:38:10 +0900

    課題提出　PHP　ー　条件文

--- taskArray/taskArray.php
+++ taskArray/taskArray.php
@@ -1,6 +1,29 @@
 <?php
+    $averageSalaryArray = [
+        2018 => 4410000,
+        2017 => 4320000,
+        2016 => 4220000,
+        2015 => 4200000,
+        2014 => 4150000
+    ];
 
+    $allArray = array_sum($averageSalaryArray);
+    $element = count($averageSalaryArray);
+    $Array5 = $allArray / $element;
+
+    $score = [
+        'A' => 80,
+        'B' => 55,
+        'C' => 61,
+        'D' => 76,
+        'E' => 34,
+        'F' => 93,
+    ];
+    $allScore = array_sum($score);
+    $element2 = count($score);
+    $alphabetScore = $allScore / $element2;
 ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -10,14 +33,6 @@
     <title>taskArray</title>
 </head>
 <body>
-        <?php //task 1 - 1
-            $averageSalaryArray = [
-                2018 => 4410000,
-                2017 => 4320000,
-                2016 => 4220000,
-                2015 => 4200000,
-                2014 => 4150000
-        ]; ?>
     <p>
         <?php 
             echo("<pre>");
@@ -26,38 +41,17 @@
             
         ?>
     </p>
-        <?php //task 1 - 2 
-            $allArray = array_sum($averageSalaryArray);
-            $Array5 = $allArray / 5;
-        ?>
-    <p>
+    <p> 
         <?php echo("過去五年間の平均年収は" . $Array5 . "です"); ?>
     </p>
-    <?php //task 2 - 1
-            $score = [
-                'A' => 80,
-                'B' => 55,
-                'C' => 61,
-                'D' => 76,
-                'E' => 34,
-                'F' => 93
-        ]; ?>
     <p>
         <?php 
             echo("<pre>");
-            var_dump($score);
+            var_dump($score);   
             echo("</pre><br>");
         ?>
     </p>
-    <?php //task 2 - 2 
-            $allScore = array_sum($score);
-            $alphabetScore = $allScore / 6;
-        ?>
-    <p>
-        <?php
-            echo("平均点は" . $alphabetScore . "です");
-        ?>
-    </p>
+    <p> <?php echo("平均点は" . $alphabetScore . "です"); ?> </p>
     
 </body>
 <body>
