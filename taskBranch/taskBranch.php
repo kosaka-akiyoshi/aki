ea127d45113750fdcfc848dfde5981395f912288
kosaka-akiyoshi <knzm25813@gmail.com>
2020-03-27 14:30:28 +0900

    課題再提出　PHP　ー　条件文

--- taskBranch/taskBranch.php
+++ taskBranch/taskBranch.php
@@ -22,88 +22,82 @@
     <title>taskBranch</title>
 </head>
 <body>
-    <p>
-        <?php //task 1 - 1
-        if($string == $number){
-            echo("\$string100は100です。<br>");
-            } else {
-            echo("\$string100は100ではありません。<br>");
-            }
-        ?>
+<p>
+<?php //task 1 - 1
+    if($string == $number){
+        echo("\$string100は100です。<br>");
+    } else {
+        echo("\$string100は100ではありません。<br>");
+    }
+?>
 
-        <?php //task 1 - 2
-        if($string === $number){
-            echo("\$string100は100です。<br>");
-            } else {
-            echo("\$string100は100ではありません。<br>");
-            } 
-        ?>
+<?php //task 1 - 2
+    if($string === $number){
+        echo("\$string100は100です。<br>");
+    } else {
+        echo("\$string100は100ではありません。<br>");
+    } 
+?>
 
-        <?php //task 1 - 3
-            if($string > $number){
-            echo("\$string100は101以上です。<br>");
-            } else {
+<?php //task 1 - 3
+    if($string > $number){
+        echo("\$string100は101以上です。<br>");
+    } else {
         echo("\$string100は101以上ではありません。<br>");
-            } 
-        ?>
+    } 
+?>
 
-        <?php //task 1 - 4
-        if($string >= $number){
-            echo("\$string100は100以上です。<br>");
-            } else {
-            echo("\$string100は100以上ではありません。<br>");
-            } 
+<?php //task 1 - 4
+    if($string >= $number){
+        echo("\$string100は100以上です。<br>");
+    } else {
+        echo("\$string100は100以上ではありません。<br>");
+    } 
+?>
+</p>
+<p> 
+<?php //task 2 - 1
+    echo("A君の平均点は" . $Grade . "点です。<br>"); 
+?>
+<?php //task 2 - 2
+    if($Grade >= $allGrade){
+        echo("A君の平均点は67.3以上です。<br>");
+    } else {
+        echo("A君の平均点は67.3未満です。<br>");
+    } 
+?>
+</p>
+<p> 
+<?php //task 3 - 1
+    switch($case){
+        case true:
+            echo 'bool<br>';
+            break;
+        case 1:
+            echo 'int<br>';
+            break; 
+        case '1':
+            echo 'string<br>';
+            break;
+        default:
+            echo 'other<br>';
+    }
         ?>
-    </p>
-    <p> 
-        <?php //task 2 - 1
-        echo("A君の平均点は" . $Grade . "点です。<br>"); 
-        ?>
-        <?php //task 2 - 2
-        if($Grade >= $allGrade){
-            echo("A君の平均点は67.3以上です。<br>");
-            } else {
-            echo("A君の平均点は67.3未満です。<br>");
-            } 
-        ?>
-    </p>
-    <p> 
-        <?php //task 3 - 1
-            switch($case){
-                case true:
-                    echo 'bool<br>';
-                    break;
-
-                case 1:
-                    echo 'int<br>';
-                    break; 
-            
-                case '1':
-                    echo 'string<br>';
-                    break;
-            
-                default:
-                    echo 'other<br>';
-            }
-        ?>
-        <?php //task 3 - 2
-            switch($case){
-                case $case === true: //false 
-                    echo 'bool<br>';
-                    break;
-
-                case $case === 1: //false
-                    echo 'int<br>';
-                    break; 
-        
-                case $case === '1'://true
-                    echo 'string<br>';
-                    break;
-        
-                default:
-                    echo 'other<br>';
-                } 
-        ?>
-    </p>
+<?php //task 3 - 2
+    switch($case){
+        case $case === true: //false 
+            echo 'bool<br>';
+            break;
+        case $case === 1: //false
+            echo 'int<br>';
+            break; 
+        case $case === '1'://true
+            echo 'string<br>';
+            break;
+        default:
+            echo 'other<br>';
+    } 
+?>
+</p>
 </body>
 </html>
