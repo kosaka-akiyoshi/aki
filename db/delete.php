67ca16091e7e79b2f16e76a20631413baedd2c28
kosaka-akiyoshi <knzm25813@gmail.com>
2020-04-06 16:34:05 +0900

    課題提出 PHP ー　データ削除

---
+++ db/delete.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>taskFunc</title>
+</head>
+<body>
+<p>
+    <?php 
+        echo(//task1
+            delete from heritage where name in('小笠原諸島','姫路城'); 
+        ); 
+    ?>
+</p>
+    
+</body>
+<body>
+</body>
+</html>
