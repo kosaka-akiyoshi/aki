f9d0383ddd803f10783e3308c2dea034395c956e
kosaka-akiyoshi <knzm25813@gmail.com>
2020-04-08 17:10:51 +0900

    課題再提出　PHP　－　ループ文

--- taskloop/taskloop.php
+++ taskloop/taskloop.php
@@ -72,6 +72,7 @@
         ],
     ];   
 ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -82,120 +83,67 @@
 </head>
 <body>
 <p>
-    <?php 
+<?php 
         //task1
         foreach($fruitstranslate as $key => $value){
             echo(
-            '英語では' . $key . 'と表示され、
-            日本語では' . $value['japanese'] . '、
-            価格は' . $value['price' ] . '円です<br>'
-        );
-    }  
+            '英語では' . $key 
+            . 'と表示され、日本語では' . $value['japanese'] 
+            . '、価格は' . $value['price' ] . '円です</br>'
+            );
+        }  
+?>
+</p>
+<p> 
+    <table border = 1 >
+    <?php  
+    //1行目
+    foreach ($grade as $tensu_array){
+        $kyouka_namae_array = array_keys($tensu_array);
+    }
+    echo("<tr><td>名前</td>");
+    foreach($kyouka_namae_array as $kyouka){
+        echo("<td>".$kyouka."</td>");
+    }
+    echo("<td>平均</td></tr>");
+    $totalPasonalPoint = 0;
+    $menberCount = count($grade);
+    //2から7行目
+    foreach($grade as $menber => $tensu_array){
+        $kokugo_array = array_column($grade, '国語');
+        $sugaku_array = array_column($grade, '数学');
+        $rika_array = array_column($grade, '理科');
+        $syakai_array = array_column($grade, '社会');
+        $eigo_array = array_column($grade, '英語');
+        echo("<tr><td>".$menber."</td>");
+
+        foreach($tensu_array as $tensu){
+            echo("<td>".$tensu."</td>");
+        }
+        $memberPoint = array_sum($tensu_array);
+        $subjectElement = count($tensu_array);
+        $menberAveragePoint = round($memberPoint / $subjectElement, 1);
+        $totalPasonalPoint += $menberAveragePoint;
+        echo('<td>'.$menberAveragePoint.'</td></tr>');
+    }
+    //8行目
+    echo("<tr><td>平均</td>");
+    $kyouka_array = [];
+    $kyouka_array[] = $kokugo_array;
+    $kyouka_array[] = $sugaku_array;
+    $kyouka_array[] = $syakai_array;
+    $kyouka_array[] = $rika_array;
+    $kyouka_array[] = $eigo_array;
+
+    foreach($kyouka_array as $kyouka_tensu_array){
+        $kyoukaPoint = array_sum($kyouka_tensu_array);
+        $kyoukaAveragePoint = round($kyoukaPoint / $menberCount, 1);
+        echo("<td>".$kyoukaAveragePoint."</td>");
+    }
+    $averageAverage = round($totalPasonalPoint / $menberCount, 1);
+    echo("<td>".$averageAverage."</td></tr>");
     ?>
-</p>
-<p>
-    <?php
-        //task2-1
-        foreach($grade as $menber => $tensu_array){
-            $memberPoint = array_sum($tensu_array);
-            echo($menber.'の点数 '.$memberPoint.'　');    
-        };
-
-        echo('</br>'); 
-        //task2-2
-        foreach($grade as $menber => $tensu_array){
-            $memberPoint = array_sum($tensu_array);
-            $subjectElement = count($tensu_array);
-            $mAveragePoint = round($memberPoint / $subjectElement, 1);
-            echo($menber.'の平均 '.$mAveragePoint.'　');
-        };
-        
-        echo('</br>');  
-        //task2-3
-        $kokugo_array = [];
-        $sugaku_array = [];
-        $rika_array = [];
-        $syakai_array = [];
-        $eigo_array = [];
-        $namae_array = [];
-
-        foreach($grade as $menber => $tensu_array){
-            foreach($tensu_array as $kyouka => $tensu) {
-                if ($kyouka == "国語"){
-                $kokugo_array[] = $tensu;
-                }
-                elseif($kyouka == "数学"){
-                $sugaku_array[] = $tensu;
-                }
-                elseif($kyouka == "社会"){
-                $syakai_array[] = $tensu;
-                }
-                elseif($kyouka == "理科"){
-                $rika_array[] = $tensu;
-                }
-                elseif($kyouka == "英語"){
-                $eigo_array[] = $tensu;
-                }
-                $namae_array[] = $kyouka;
-                }
-            }; 
-        
-        //キーと値を入れ替えた二次元配列を作成
-        $array_namae = array_flip($namae_array);
-        $replacements = array(
-            '国語' => $kokugo_array, '数学' => $sugaku_array, 
-            '社会' => $syakai_array, '理科' => $rika_array, '英語' => $eigo_array,
-        );
-        $subject_array = array_replace($array_namae, $replacements);
-
-        $kyouka_average_array = [];
-        foreach($subject_array as $kyouka2 => $array_tensu){
-            $kyoukaPoint = array_sum($array_tensu);
-            $kyoukaElement = count($array_tensu);
-            $averageKyoukaPoint = round($kyoukaPoint / $kyoukaElement, 1);
-            echo($kyouka2.'の平均 '.$averageKyoukaPoint.'　');
-            
-            //task2-4(表作成)
-            //教科の平均点の配列を作成
-            $kyouka_average_array[] = $averageKyoukaPoint;
-        };
-
-        //二次元配列に名前とメンバーの平均を追加
-        $grade2 = [];
-        foreach($grade as $menber => $tensu_array){
-            $allMemberPoint = array_sum($tensu_array);
-            $subjectElement = count($tensu_array);
-            $averageMemberPoint = $allMemberPoint / $subjectElement;
-            array_unshift($tensu_array, $menber);
-            array_push($tensu_array, $averageMemberPoint);
-            $grade2[] = $tensu_array;
-            //教科の平均点をさらに平均すると71.3になったのでメンバーの平均点を平均
-            $member_average_array[] = $averageMemberPoint;
-        };
-
-        //教科の名前と平均を追加（すべての平均点はメンバーの平均点を平均）
-        $ue_array = ['名前','国語','数学','社会','理科','英語','平均'];
-        $allAverage = array_sum($member_average_array);
-        $averageElement = count($member_average_array);
-        $averageAverage = round($allAverage / $averageElement, 1);
-        array_push($kyouka_average_array, $averageAverage);
-        array_unshift($kyouka_average_array, '平均');
-    
-        array_unshift($grade2, $ue_array);
-        array_push($grade2, $kyouka_average_array); 
-    ?>  
-</p>
-<table border = 1 >
-    <!--行の処理内容-->
-    <?php foreach ($grade2 as $menber => $tensu_array){ ?>
-    <tr>
-    <!--列の処理内容-->
-    <?php foreach($tensu_array as $kyouka => $tensu){ ?>
-    <!--表示-->　
-    <td> <?php echo("$tensu") ?> </td>
-    <?php } ?>
-    </tr>
-    <?php } ?>
 </table>
+</p>
 </body>
 </html>
